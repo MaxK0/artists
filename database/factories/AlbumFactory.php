def62169ba6cf643925f0c66b59006363537f6e8
MaxK0 <maxkoryakov1@gmail.com>
2024-12-04 14:47:16 +0500

    Сделаны фабрики

---
+++ database/factories/AlbumFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Artist;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Album>
+ */
+class AlbumFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'release_year' => fake()->year,
+            'artist_id' => Artist::factory()
+        ];
+    }
+}
