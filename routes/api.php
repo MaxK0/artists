a9d2f40044126e226af0ea6ac735d99777e2fea8
ваше_имя <innusya04@mail.ru>
2024-12-05 13:40:18 +0500

    Добавлен префикс имени

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(base_path('routes/api_v1.php'));
+Route::name('api.')->group(function () {
+    Route::prefix('v1')->name('v1.')->group(base_path('routes/api_v1.php'));
+});
+
 
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,4 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\ArtistController;
 use Illuminate\Support\Facades\Route;
 
+Route::apiResource('artists', ArtistController::class);
